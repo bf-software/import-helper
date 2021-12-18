efaa4fcb3052cd55b78f4ebfcc5d8834a1300416
friedrb <42466550+friedrb@users.noreply.github.com>
2021-12-18 08:28:40 -0500

    removed out-tests-bundle from repository

--- out-tests-bundle/images/Microsoft/attribution.js
+++
@@ -1,1 +0,0 @@
-exports={author:"Microsoft",authorURL:"https://microsoft.com/",distributor:"Microsoft",distributorURL:"https://github.com/microsoft/vscode-codicons",license:"CC BY 4.0",licenseURL:"https://creativecommons.org/licenses/by/4.0/"};
